8359796aebc578db577507fc4d091b02465add6d
Junichirou OKU <jun1ro1@me.com>
2020-11-14 22:03:10 +0900

    Initial Commit

---
+++ TresorMockup4/TresorMockup4App.swift
@@ -0,0 +1,20 @@
+//
+//  TresorMockup4App.swift
+//  TresorMockup4
+//
+//  Created by OKU Junichirou on 2020/11/14.
+//
+
+import SwiftUI
+
+@main
+struct TresorMockup4App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
